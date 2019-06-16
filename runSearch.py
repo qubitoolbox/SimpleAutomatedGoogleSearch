be1f9cd3bdd19c4d215b5763d10b4c21012366d8
qubitAI <osmanmorales@ymail.com>
2019-06-16 11:37:30 -0400

    Update runSearch.py

--- runSearch.py
+++ runSearch.py
@@ -2,3 +2,6 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+import numpy as np
+import time	
